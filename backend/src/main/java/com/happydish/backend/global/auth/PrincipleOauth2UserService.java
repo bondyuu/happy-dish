da22de866fdfd3dd549b1da42438a671b88427e2
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-03-16 14:31:39 +0900

    Fix: OAuth 로그인시 저장되는 값 변경

--- backend/src/main/java/com/happydish/backend/global/auth/PrincipleOauth2UserService.java
+++ backend/src/main/java/com/happydish/backend/global/auth/PrincipleOauth2UserService.java
@@ -10,6 +10,7 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -21,15 +22,16 @@ public class PrincipleOauth2UserService extends DefaultOAuth2UserService {
 
     // 구글로부터 받은 userRequest 데이터에 대한 후처리되는 함수
     @Override
+    @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         //구글 로그인 버튼 -> 구글 로그인 -> 완료 -> code 리턴(Oauth-Client 라이브러리) -> AccessToken요청
         //userRequest 정보 -> loadUser 함수 호출 -> 회원 프로필
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
-        String provider = userRequest.getClientRegistration().getClientId(); //google
+        String provider = userRequest.getClientRegistration().getRegistrationId();
         String providerId = oAuth2User.getAttribute("sub");
         String email = oAuth2User.getAttribute("email");
-        String username = provider + "_" + providerId; // google_1234
+        String username = oAuth2User.getAttribute("name");
         String password = "@#$*892743q#$984sdfhsdf)#@$*(sdf";
 
         Optional<User> userEntity = userRepository.findByEmail(email);
