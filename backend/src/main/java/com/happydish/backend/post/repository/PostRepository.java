f9ae6f04392acf170f6e932c310264eba2ceb18d
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-03-23 17:10:42 +0900

    Refator: Comment도 Status를 사용할 수 있도록 일반화

--- backend/src/main/java/com/happydish/backend/post/repository/PostRepository.java
+++ backend/src/main/java/com/happydish/backend/post/repository/PostRepository.java
@@ -1,12 +1,12 @@
 package com.happydish.backend.post.repository;
 
 import com.happydish.backend.post.model.Post;
-import com.happydish.backend.post.model.PostStatus;
+import com.happydish.backend.post.model.Status;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Page<Post> findAllByStatus(PostStatus status, Pageable pageable);
-    Page<Post> findAllByTitleContainingAndStatus(String title, PostStatus status, Pageable pageable);
+    Page<Post> findAllByStatus(Status status, Pageable pageable);
+    Page<Post> findAllByTitleContainingAndStatus(String title,Status status, Pageable pageable);
 }
