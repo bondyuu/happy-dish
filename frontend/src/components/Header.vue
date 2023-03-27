ad31d182b74c935aa2cbb2d616b2cbcd3ad1ba5f
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-03-27 15:21:32 +0900

    Feat: Header 추가

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="header-container">
+    <div class="logo">
+      <h2>HAPDI</h2>
+    </div>
+    <div class="nav-container">
+      <router-link to="">게시글</router-link> |
+      <router-link to="">게시글</router-link> |
+      <router-link to="">게시글</router-link> |
+      <router-link to="">게시글</router-link>
+    </div>
+    <div class="login">
+      <router-link to="/login"><b-button variant="outline-primary">Login</b-button></router-link>
+      <router-link to="/sign-up"><b-button variant="outline-primary">Sign-Up</b-button></router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+.header-container {
+  display: flex;
+  margin-top: 20px;
+  margin-left: 4%;
+  width: 92%;
+}
+.nav-container {
+  margin-left: 3%;
+  margin-right: 60%;
+}
+</style>
